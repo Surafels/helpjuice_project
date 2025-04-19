e4578ee8d12e800a187ff296c509e21d290de0c1
Surafels <ssurafel4@gmail.com>
2025-04-19 12:28:05 +0300

    Add footer

---
+++ app/components/footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <div className="relative">
+            {/* <div className=" flex justify-end  w-[30px] h-[30px]  rounded-full  ">?</div> */}
+            <span className=" flex justify-center border border-[#E5E7EB] w-1/2 h-0.5 m-auto mb-4 ">
+            </span>
+        </div>
+    )
+}
+
+export default Footer;
