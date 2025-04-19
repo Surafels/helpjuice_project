81270cab7f933f2cfedc818df67f56438769d591
Surafels <ssurafel4@gmail.com>
2025-04-19 14:18:19 +0300

    Update help-circle

--- app/components/footer.js
+++ app/components/footer.js
@@ -1,8 +1,13 @@
+import circleCheck from '../assets/circle-help.svg'
+import Image from 'next/image';
 const Footer = () => {
     return (
-        <div className="relative">
-            {/* <div className=" flex justify-end  w-[30px] h-[30px]  rounded-full  ">?</div> */}
-            <span className=" flex justify-center border border-[#E5E7EB] w-1/2 h-0.5 m-auto mb-4 ">
+        <div className="">
+            <div className=" flex justify-end pr-3">
+                <Image className='bg-[#F3F4F6] rounded-[2px]' src={circleCheck} alt="circle check" layout="fixed" />
+            </div>
+
+            <span className=" flex justify-center border border-[#E5E7EB] shadow-[0_4px_8px_0_rgba(0,0,0,0.2),0_6px_20px_0_rgba(0,0,0,0.19)]  w-1/2 h-0.5 m-auto mb-4 ">
             </span>
         </div>
     )
